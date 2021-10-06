9ca15baccc5f8fd44e396717a61d76092a7f544b
hkapoor2001 <hkapoor2001@gmail.com>
2021-10-06 05:22:37 -0400

    timing on animations

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,5 +2,8 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#experienceart",{duration:1, alpha:.5, y:"-=200", x:"=+5000", scale:3, ease: "bounce"}
+mainTL.from("#logo",{duration:1.3, alpha:1, y:"+=50", x:-"=0", scale:1, ease:"bounce"})
 
+mainTL.from("#experienceart",{duration:.8, alpha:.5, y:"-=200", x:"=+5000", scale:3})
+
+mainTL.from("#herologo",{duration:1.2, alpha:0, y:"+=100", x:"=+500", scale:1.5, ease:"bounce"})
