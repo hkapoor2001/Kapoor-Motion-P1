82b1efd9006705e8426f49bb91c141ecd9e1df50
hkapoor2001 <hkapoor2001@gmail.com>
2021-11-03 11:04:17 -0400

    added paintings 2-5

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,188 +7,143 @@ gsap.registerPlugin(GSDevTools, DrawSVGPlugin);
 
 const mainTL = gsap.timeline();
 
-function innerframedraw(){
+function innerframefade(){
     const tl = gsap.timeline();
     tl.from("#innerframe",{duration:0.5, alpha:0})
     .to("#innerframe",{duration:0.5, alpha:1})
-    //gsap.from("#innerframe",{duration: 2.2, drawSVG: "0%", ease: "none"})
     return tl;
 }
 
-
-
-function drawpaintinglines(){
+function outerframedraw(){
     const tl = gsap.timeline();
-    tl.from(".painting1class",{duration:0.25, drawSVG: "0%", stagger:"-50%" , ease: "none"})
-    .to("#painting1",{rotation: "90_short", ease: "none", transformOrigin: "50% 50%"})
-    return tl;
+    tl.from("#outerframe",{duration:10, drawSVG: "0", ease:"none"})
 }
 
 
-function rotate(){
+function drawpaintinglines1(){
     const tl = gsap.timeline();
-    tl.from(".painting1class",{duration:0.25, drawSVG: "0%", ease: "none"})
-    .to("#painting1",{rotation: "90_short", ease: "none", transformOrigin: "50% 50%"})
 
-    tl.to("#innerframe",{rotation: "90_short", ease: "none", transformOrigin: "50% 50%"})
+    tl.from(".painting1",{duration:1, drawSVG: "0", ease:"none"})
 
-    tl.to("#outerframe",{rotation: "90_short", ease: "none", transformOrigin: "50% 50%"})
     return tl;
 }
 
-// function drawpainting1line1(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line1",{duration:1, drawSVG: "0%", delay:2.5, ease: "none"})
-//     return tl;
-// }
 
-// function drawpainting1line2(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line2",{duration:1, drawSVG: "0%", delay:3.5, ease: "none"})
-//     return tl;
-// }
+function rotate1(){
+    const tl = gsap.timeline();
 
-// function drawpainting1line3(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line3",{duration:1, drawSVG: "0%", delay:4.5, ease: "none"})
-//     return tl;
-// }
+    tl.from("#painting1",{rotation: "0", alpha: 1})
 
-// function drawpainting1line4(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line4",{duration:1.5, drawSVG: "0%", delay:5.5, ease: "none"})
-//     return tl;
-// }
+    tl.to("#painting1",{rotation: "90_cw", ease: "none", transformOrigin: "55.5% 50%", alpha: 0},"1same")
 
-// function drawpainting1line5(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line5",{duration:.6, drawSVG: "0%", delay:7, ease: "none"})
-//     return tl;
-// }
+    tl.to("#innerframe",{rotation: "90_cw", ease: "none", transformOrigin: "50% 50%"},"1same")
 
-// function drawpainting1line6(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line6",{duration:.6, drawSVG: "0%", delay:5.5, ease: "none"})
-//     return tl;
-// }
+    tl.to("#outerframe",{rotation: "90_cw", ease: "none", transformOrigin: "50% 50%"},"1same")
 
-// function drawpainting1line7(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line7",{duration:.6, drawSVG: "0%", delay:6.1, ease: "none"})
-//     return tl;
-// }
+    return tl;
+}
 
-// function drawpainting1line8(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line8",{duration:.6, drawSVG: "0%", delay:6.7, ease: "none"})
-//     return tl;
-// }
+function drawpaintinglines2(){
+    const tl = gsap.timeline();
+
+    tl.from(".painting2",{duration:1, drawSVG: "0%", ease:"none"})
+
+    return tl;
+}
+
+function rotate2(){
+    const tl = gsap.timeline();
+
+    tl.from("#painting2",{rotation: "0", alpha: 1})
+
+    tl.to("#painting2",{rotation: "90_cw", ease: "none", transformOrigin: "55.5% 50%", alpha: 0},"2same")
+
+    tl.to("#innerframe",{rotation: "180_cw", ease: "none", transformOrigin: "50% 50%"},"2same")
+
+    tl.to("#outerframe",{rotation: "180_cw", ease: "none", transformOrigin: "50% 50%"},"2same")
+
+    return tl;
+}
 
 
-// function drawpainting1line9(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line9",{duration:.6, drawSVG: "0%", delay:7.3, ease: "none"})
-//     return tl;
-// }
+function drawpaintinglines3(){
+    const tl = gsap.timeline();
 
-// function drawpainting1line10(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line10",{duration:.6, drawSVG: "0%", delay:7.9, ease: "none"})
-//     return tl;
-// }
+    tl.from(".painting3",{duration:1, drawSVG: "0%", ease:"none"})
 
-// function drawpainting1line11(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line11",{duration:.6, drawSVG: "0%", delay:8.5, ease: "none"})
-//     return tl;
-// }
+    return tl;
+}
 
-// function drawpainting1line12(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line12",{duration:.6, drawSVG: "0%", delay:9.1, ease: "none"})
-//     return tl;
-// }
+function rotate3(){
+    const tl = gsap.timeline();
 
-// function drawpainting1line13(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line13",{duration:.6, drawSVG: "0%", delay:9.7, ease: "none"})
-//     return tl;
-// }
+    tl.from("#painting3",{rotation: "0", alpha: 1})
 
-// function drawpainting1line14(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line14",{duration:.6, drawSVG: "0%", delay:10.3, ease: "none"})
-//     return tl;
-// }
+    tl.to("#painting3",{rotation: "90_cw", ease: "none", transformOrigin: "55.5% 50%", alpha: 0},"3same")
 
-// function drawpainting1line15(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line15",{duration:.6, drawSVG: "0%", delay:10.9, ease: "none"})
-//     return tl;
-// }
+    tl.to("#innerframe",{rotation: "270_cw", ease: "none", transformOrigin: "50% 50%"},"3same")
 
-// function drawpainting1line16(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line16",{duration:.6, drawSVG: "0%", delay:11.5, ease: "none"})
-//     return tl;
-// }
+    tl.to("#outerframe",{rotation: "270_cw", ease: "none", transformOrigin: "50% 50%"},"3same")
 
-// function drawpainting1line17(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line17",{duration:.6, drawSVG: "0%", delay:12.1, ease: "none"})
-//     return tl;
-// }
+    return tl;
+}
 
-// function drawpainting1line18(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#line18",{duration:.6, drawSVG: "0%", delay:12.1, ease: "none"})
-//     return tl;
-// }
+function drawpaintinglines4(){
+    const tl = gsap.timeline();
 
-// function rotateinnerframe(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#innerframe",{rotate: "90, 50, 50", delay:12.1, ease: "none", transformOrigin:"50% 50%"})
-//     return tl;
-// }
+    tl.from(".painting4",{duration:1, drawSVG: "0%", ease:"none"})
 
-// function rotateouterframe(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#outerframe",{rotate: "90, 50, 50", delay:12.1, ease: "none", transformOrigin:"50% 50%"})
-//     return tl;
-// }
+    return tl;
+}
 
-// function rotatepainting1(){
-//     const tl = gsap.timeline();
-    
-//     gsap.from("#painting1",{rotation: "90", delay:12.1, ease: "none", transformOrigin:"50% 50%",xPercent:-50, yPercent:-50})
-//     return tl;
-// }
+function rotate4(){
+    const tl = gsap.timeline();
+
+    tl.from("#painting4",{rotation: "0", alpha: 1})
+
+    tl.to("#painting4",{rotation: "90_cw", ease: "none", transformOrigin: "55.5% 50%", alpha: 0},"4same")
+
+    tl.to("#innerframe",{rotation: "360_cw", ease: "none", transformOrigin: "50% 50%"},"4same")
+
+    tl.to("#outerframe",{rotation: "360_cw", ease: "none", transformOrigin: "50% 50%"},"4same")
+
+    return tl;
+}
+
+function drawpaintinglines5(){
+    const tl = gsap.timeline();
+
+    tl.from(".painting5",{duration:1, drawSVG: "0%", ease:"none"})
+
+    return tl;
+}
+
+function rotate5(){
+    const tl = gsap.timeline();
+
+    tl.from("#painting5",{rotation: "0", alpha: 1})
+
+    tl.to("#painting5",{rotation: "90_cw", ease: "none", transformOrigin: "55.5% 50%", alpha: 0},"5same")
+
+    tl.to("#innerframe",{rotation: "90_cw", ease: "none", transformOrigin: "50% 50%"},"5same")
+
+    tl.to("#outerframe",{rotation: "90_cw", ease: "none", transformOrigin: "50% 50%"},"5same")
+
+    return tl;
+}
 
 
 
-
-mainTL.add(innerframedraw())
-      .add(rotate())
-
-
+mainTL.add(innerframefade())
+      .add (outerframedraw())
+      .add(drawpaintinglines1())
+      .add(rotate1())
+      .add(drawpaintinglines2())
+      .add (rotate2())
+      .add(drawpaintinglines3())
+      .add (rotate3())
+      .add(drawpaintinglines4())
+      .add (rotate4())
+      .add(drawpaintinglines5())
+      .add (rotate5())
 GSDevTools.create();
